71badcd44517399a0375ca7050d8226217c25d62
Hafizul <islamhafizul158@gmail.com>
2024-01-25 08:42:24 +0600

    provide return type

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,36 +7,36 @@ import { Authorization } from './guards/authorization.guard';
 Request
 @Controller('auth')
 export class AuthController {
-    constructor(private authService:AuthService){}
+    constructor(private authService: AuthService) {}
 
     @UseGuards(LocalAuthGuard)
     @Post('login')
-    async login(@Request() req) {
-      return await this.authService.login(req.user);
+    async login(@Request() req): Promise<any> {
+        return await this.authService.login(req.user);
     }
-  
+
     @Get('validate')
-    validateUser(username:string,password:string) {
-      return this.authService.validateUser({username,password});
+    validateUser(username: string, password: string): Promise<any> {
+        return this.authService.validateUser({ username, password });
     }
-    
+
     @UseGuards(JwtGuard)
     @UseGuards(RoleGuard) // Apply both role and permission guards
-    @Authorization(['admin','user'], ['user.create','user.get']) // Combine roles and permissions
+    @Authorization(['admin', 'user'], ['user.create', 'user.get']) // Combine roles and permissions
     @Get('user-create')
-    async createUser(){
-      return this.authService.createUser()
-    }   
+    async createUser(): Promise<any> {
+        return this.authService.createUser();
+    }
     @Get('user-update')
-    async updateUser(){
-      return this.authService.createUser()
-    } 
+    async updateUser(): Promise<any> {
+        return this.authService.createUser();
+    }
     @Get('user-get')
-    async getUser(){
-      return this.authService.createUser()
+    async getUser(): Promise<any> {
+        return this.authService.createUser();
     }
     @Get('user-get-all')
-    async getUsers(){
-      return this.authService.createUser()
+    async getUsers(): Promise<any> {
+        return this.authService.createUser();
     }
 }
